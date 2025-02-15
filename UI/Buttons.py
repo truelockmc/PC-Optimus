fbfb1969c295da36616f9df4d9b5d1d7b758a8d7
truelockmc <168660625+truelockmc@users.noreply.github.com>
2025-02-15 23:39:55 +0100

    changed "update all apps" functionality
    - added winget source update to the command
    - added ``` --include-unknown --accept-package-agreements --accept-source-agreements --force --disable-interactivity --wait ``` as parameters to the winget upgrade command

--- UI/Buttons.py
+++ UI/Buttons.py
@@ -51,7 +51,7 @@ def create_buttons(root, show_main_menu, show_info_menu, show_clean_menu, show_u
         "defragment_button": tk.Button(root, text="Defragment", command=defragment, **button_style),
         "storage_diagonistics_button": tk.Button(root, text="Storage Diagnostics", command=lambda: run_command("MdSched.exe", "Successfully ran Windows Storage Diagnostics."), **button_style),
         "update_button": tk.Button(root, text="Update", command=show_update_menu, **button_style),
-        "update_apps_button": tk.Button(root, text="Update Apps", command=lambda: run_window_admin_command("title Updater && cls && winget upgrade --all", "Winget initiated successfully.", root), **button_style),
+        "update_apps_button": tk.Button(root, text="Update Apps", command=lambda: run_window_admin_command("title Updater && winget source update && cls && winget upgrade --all --include-unknown --accept-package-agreements --accept-source-agreements --force --disable-interactivity --wait", "Winget initiated successfully.", root), **button_style),
         "windows_update_button": tk.Button(root, text="Windows Update", command=lambda: run_command("wuauclt /detectnow /updatenow", "Windows update initiated successfully."), **button_style),
         "repair_connection_button": tk.Button(root, text="Repair Connection", command=lambda: run_admin_command("ipconfig /flushdns && netsh winsock reset && ipconfig /release && ipconfig /renew", "Successfully reset Network and tried to fix Connection Problems.", root), **button_style),
         "driver_update_button": tk.Button(root, text="Driver Update", command=lambda: run_command("wuauclt /detectnow /updatenow", "Drivers updated successfully."), **button_style),
